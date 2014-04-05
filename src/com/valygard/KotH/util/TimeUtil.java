9cc46d090b9562c599218f5e086519caa371bd8d
AoHRuthless <kumara0398@gmail.com>
2014-04-05 18:14:06 -0400

    Fixed formatting

--- src/com/valygard/KotH/util/TimeUtil.java
+++ src/com/valygard/KotH/util/TimeUtil.java
@@ -9,11 +9,11 @@ package com.valygard.KotH.util;
 public class TimeUtil
 {
 	public static String formatIntoHHMMSS(int secs) {
-	int remainder = secs % 3600;
-	int minutes = remainder / 60;
-	int seconds = remainder % 60;
+		int remainder = secs % 3600;
+		int minutes = remainder / 60;
+		int seconds = remainder % 60;
 
-	return new StringBuilder().append(minutes).append(":").append(seconds < 10 ? "0" : "").append(seconds).toString();
+		return new StringBuilder().append(minutes).append(":").append(seconds < 10 ? "0" : "").append(seconds).toString();
 	}
 
 }
