35c696f5f8569bfced0a13f4ee986b7f74315436
AoHRuthless <kumara0398@gmail.com>
2014-04-12 13:46:31 -0400

    Now saves enchantments, lore, names, etc..

--- src/com/valygard/KotH/ArenaPlayer.java
+++ src/com/valygard/KotH/ArenaPlayer.java
@@ -5,11 +5,15 @@
 package com.valygard.KotH;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.potion.PotionEffect;
 
 /**
@@ -18,7 +22,8 @@ import org.bukkit.potion.PotionEffect;
  */
 public class ArenaPlayer {
 	private Player player;
-	private ItemStack[] contents, armorContents;
+	private ItemStack[] contents;
+	private ItemStack head, chest, legs, feet;
 	private Location loc = null;
 	private double health;
 	private int level, food;
@@ -31,37 +36,75 @@ public class ArenaPlayer {
 	 * Constructor to initialize all the variables.
 	 */
 	public ArenaPlayer(Player player) {
-		this.player 		= player;
-		this.contents 		= player.getInventory().getContents();
-		this.armorContents	= player.getInventory().getArmorContents();
-		this.loc 			= player.getLocation();
-		this.mode 			= player.getGameMode();
-		this.potions 		= player.getActivePotionEffects();
-		this.food			= player.getFoodLevel();
-		this.health			= player.getHealth();
-		this.level			= player.getLevel();
-		this.exp			= player.getExp();
+		this.player 	= player;
+		
+		this.contents 	= player.getInventory().getContents();
+		
+		this.head		= player.getInventory().getHelmet();
+		this.chest		= player.getInventory().getChestplate();
+		this.legs		= player.getInventory().getLeggings();
+		this.feet		= player.getInventory().getBoots();
+		
+		this.loc 		= player.getLocation();
+		this.mode 		= player.getGameMode();
+		this.potions 	= player.getActivePotionEffects();
+		
+		this.food		= player.getFoodLevel();
+		this.health		= player.getHealth();
+		this.level		= player.getLevel();
+		this.exp		= player.getExp();
 	}
 
 	/**
 	 * Restores health, food, and experience when a player
 	 * exits the arena, as per the stored data.
 	 */
+	@SuppressWarnings("deprecation")
 	public void restoreData() {
 		player.setHealth(health);
 		player.setFoodLevel(food);
+		
 		player.setLevel(level);
 		player.setExp(exp);
 		player.teleport(loc);
 		
 		for (ItemStack i : contents) {
-			if (i != null)
-				player.getInventory().addItem(i);
+			parseItem(i);
+			player.getInventory().setItem(player.getInventory().firstEmpty(), i);
 		}
-
-		player.getInventory().setArmorContents(armorContents);
+		player.getInventory().setHelmet(parseItem(head));
+		player.getInventory().setChestplate(parseItem(chest));
+		player.getInventory().setLeggings(parseItem(legs));
+		player.getInventory().setBoots(parseItem(feet));
+		
 		player.setGameMode(mode);
 		player.addPotionEffects(potions);
+		player.updateInventory();
+	}
+	
+	private ItemStack parseItem(ItemStack i) {
+		if (i == null)
+			return null;
+		ItemMeta im = i.getItemMeta();
+		
+		Map<Enchantment, Integer> enchants;
+		String name;
+		List<String> lore;
+		
+		if (im != null) {
+			enchants 	= (im.hasEnchants() ? im.getEnchants() : null);
+			name		= (im.hasDisplayName() ? im.getDisplayName() : null);
+			lore		= (im.hasLore() ? im.getLore() : null);
+			
+			if (enchants != null)
+				i.addEnchantments(enchants);
+			if (name != null)
+				im.setDisplayName(name);
+			if (lore != null)
+				im.setLore(lore);
+			i.setItemMeta(im);
+		}
+		return i;
 	}
 
 	public Player getPlayer() {
@@ -72,8 +115,20 @@ public class ArenaPlayer {
 		return contents;
 	}
 
-	public ItemStack[] getArmorContents() {
-		return armorContents;
+	public ItemStack getHelmet() {
+		return head;
+	}
+	
+	public ItemStack getChestplate() {
+		return chest;
+	}
+	
+	public ItemStack getLeggings() {
+		return legs;
+	}
+	
+	public ItemStack getBoots() {
+		return feet;
 	}
 
 	public Location getLocation() {
