f31c7382a7043efb4ecd55b5e0ba55148189e303
AoHRuthless <kumara0398@gmail.com>
2014-05-10 09:14:30 -0400

    Fix method for getting circles and squares.

--- src/com/valygard/KotH/util/LocationUtil.java
+++ src/com/valygard/KotH/util/LocationUtil.java
@@ -4,10 +4,11 @@
  */
 package com.valygard.KotH.util;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.bukkit.Location;
+import org.bukkit.block.Block;
 
 /**
  * @author Anand
@@ -16,99 +17,51 @@ import org.bukkit.Location;
 public class LocationUtil {
 
 	/**
-	 * Gets a circular boundary of a defined center.
-	 * 
-	 * Slightly modified version of WorldEdit cylinder code:
-	 * <https://github.com/
-	 * sk89q/worldedit/blob/master/src/main/java/com/sk89q/worldedit
-	 * /EditSession.java> (Line 1249)
+	 * Get a circle based on a center.
 	 * 
 	 * @param center a location
-	 * @param radius a double
+	 * @param radius an integer
 	 * @return
 	 */
-	public static Set<Location> getCircularBoundary(Location center,
-			double radius) {
+	public static List<Block> getCircle(Location center, int radius) {
 		if (radius <= 0)
 			throw new IllegalArgumentException(
 					"The radius must be a positive integer!");
-
-		radius += 0.5;
-
-		final int ceilRadius = (int) Math.ceil(radius);
-
-		final double invRadius = 1 / radius;
-
 		int y = center.getBlockY();
 
-		Set<Location> result = new HashSet<Location>();
-
-		double nextXn = 0;
-		forX: for (int x = 0; x <= ceilRadius; ++x) {
-			final double xn = nextXn;
-			nextXn = (x + 1) * invRadius;
-			double nextZn = 0;
-			forZ: for (int z = 0; z <= ceilRadius; ++z) {
-				final double zn = nextZn;
-				nextZn = (z + 1) * invRadius;
-
-				double distanceSq = distanceSq(xn, zn);
-				if (distanceSq > 1) {
-					if (z == 0) {
-						break forX;
-					}
-					break forZ;
-				}
-
-				if (distanceSq(nextXn, zn) <= 1 && distanceSq(xn, nextZn) <= 1) {
-					continue;
-				}
-				Location q1 = new Location(center.getWorld(), x, y, z);
-				Location q2 = new Location(center.getWorld(), -x, y, z);
-				Location q3 = new Location(center.getWorld(), -x, y, -z);
-				Location q4 = new Location(center.getWorld(), x, y, -z);
-
-				result.add(q1);
-				result.add(q2);
-				result.add(q3);
-				result.add(q4);
-			}
-		}
-		return result;
+		List<Block> circle = new ArrayList<Block>();
+		for (int x = -radius; x <= radius; x++) {
+            for (int z = -radius; z <= radius; z++) {
+            	if (distanceSq(x, z) <= MathUtil.getSquare(radius)){
+            		if (distanceSq(x, z) > MathUtil.getSquare(radius - 1)) {
+            			circle.add(center.getWorld().getBlockAt(x, y, z));
+            		}
+            	}
+            }
+        }
+		return circle;
 	}
 
 	/**
-	 * In case a square boundary is of want, we can get that too.
+	 * Get a square boundary off a given location, defined as the center.
 	 * 
 	 * @param center the location
-	 * @param radius a double
+	 * @param radius an integer
 	 * @return
 	 */
-	public static Set<Location> getSquareBoundary(Location center, double radius) {
+	public static List<Block> getSquare(Location center, int radius) {
 		if (radius <= 0)
 			throw new IllegalArgumentException(
 					"The radius must be a positive integer!");
 
-		radius += 0.5;
-
-		final int ceilRadius = (int) Math.ceil(radius);
-
-		final int x = center.getBlockX();
-		final int y = center.getBlockY();
-		final int z = center.getBlockZ();
-
-		Set<Location> result = new HashSet<Location>();
-
-		for (int xn = x - ceilRadius; xn <= x + ceilRadius; xn++) {
-			for (int zn = z - ceilRadius; zn <= z + ceilRadius; zn++) {
-				boolean border = ((xn == x - ceilRadius || xn == x + ceilRadius) && (zn == z
-						- ceilRadius || zn == z + ceilRadius));
-				if (border)
-					result.add(new Location(center.getWorld(), xn, y, zn));
+		List<Block> square = new ArrayList<Block>();
+		for (int x = -radius; x <= radius; x += (radius * 2)) {
+			for (int z = -radius; z <= radius; z += (radius * 2)) {
+				square.add(center.getWorld().getBlockAt(center.getBlockX() + x,
+						center.getBlockY(), center.getBlockZ() + z));
 			}
 		}
-
-		return result;
+		return square;
 	}
 
 	/**
@@ -129,6 +82,6 @@ public class LocationUtil {
 	 * @return
 	 */
 	public static double distanceSq(double x, double z) {
-		return Math.sqrt(MathUtil.getSquare(x) + MathUtil.getSquare(z));
+		return (MathUtil.getSquare(x) + MathUtil.getSquare(z));
 	}
 }
