870333c2e3d75d46dba6df3bd30347988d74d567
AoHRuthless <kumara0398@gmail.com>
2014-05-25 15:24:09 -0400

    Better TimeUtility

--- src/com/valygard/KotH/util/TimeUtil.java
+++ src/com/valygard/KotH/util/TimeUtil.java
@@ -2,13 +2,13 @@ package com.valygard.KotH.util;
 
 /**
  * @author Anand
- *
+ * 
  */
 public class TimeUtil {
-	
+
 	/**
-	 * This method was taken from the Bukkit Forums Resources section, from a
-	 * thread on enhanced time formatting.
+	 * This method was an edited version from the Bukkit Forums Resources
+	 * section, from a thread on enhanced time formatting.
 	 * <http://forums.bukkit.org/threads/tutorial
 	 * -better-time-formatting.173185/>
 	 * 
@@ -18,10 +18,14 @@ public class TimeUtil {
 	 */
 	public static String formatIntoHHMMSS(int secs) {
 		int remainder = secs % 3600;
+
 		int minutes = remainder / 60;
 		int seconds = remainder % 60;
+		int hours = (secs / 3600) % 24;
 
-		return new StringBuilder().append(minutes).append(":").append(seconds < 10 ? "0" : "").append(seconds).toString();
+		return new StringBuilder().append(hours < 10 ? "0" : "").append(hours)
+				.append(minutes < 10 ? "0" : "").append(minutes).append(":")
+				.append(seconds < 10 ? "0" : "").append(seconds).toString();
 	}
 
 	/**
@@ -33,18 +37,23 @@ public class TimeUtil {
 	 */
 	public static String formatIntoSentence(int secs) {
 		int remainder = secs % 3600;
-		
+
 		int minutes = remainder / 60;
 		int seconds = remainder % 60;
-		
-		int days	= secs / 86400;
-		int hours	= (secs / 3600) % 24;
 
-		String fDays 	= (days > 0 ? " " + days + " day" + (days > 1 ? "s" : "") : "");
-		String fHours 	= (hours > 0 ? " " + hours + " hour" + (hours > 1 ? "s" : "") : "");
-		String fMinutes = (minutes > 0 ? " " + minutes + " minute" + (minutes > 1 ? "s" : "") : "");
-		String fSeconds	= (seconds > 0 ? " " + seconds + " second" + (seconds > 1 ? "s" : "") : "");
-		
-		return new StringBuilder().append(fDays).append(fHours).append(fMinutes).append(fSeconds).toString();
+		int days = secs / 86400;
+		int hours = (secs / 3600) % 24;
+
+		String fDays = (days > 0 ? " " + days + " day" + (days > 1 ? "s" : "")
+				: "");
+		String fHours = (hours > 0 ? " " + hours + " hour"
+				+ (hours > 1 ? "s" : "") : "");
+		String fMinutes = (minutes > 0 ? " " + minutes + " minute"
+				+ (minutes > 1 ? "s" : "") : "");
+		String fSeconds = (seconds > 0 ? " " + seconds + " second"
+				+ (seconds > 1 ? "s" : "") : "");
+
+		return new StringBuilder().append(fDays).append(fHours)
+				.append(fMinutes).append(fSeconds).toString();
 	}
 }
