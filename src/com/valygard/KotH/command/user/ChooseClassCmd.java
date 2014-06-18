846e2f1efe0db67f5e2e54fc351104d469530bc2
AoHRuthless <kumara0398@gmail.com>
2014-06-18 08:31:59 -0400

    Players can now access a configurable GUI when trying to pick a class. If 'classes-gui' is enabled in the arena settings, and the player does not have any arguments in their command, a GUI will pop open for guidance.
    Users can configure the GUI item just like any other. If more than one
    item is input the config, the GUI item will revert to a grass block. The
    display name of the item will reflect the class name, and will be green
    if the player has permission for it or gray if the player does not have
    permission. If a player clicks on a 'class' they don't have permission
    for, they will be assigned a random class.

--- src/com/valygard/KotH/command/user/ChooseClassCmd.java
+++ src/com/valygard/KotH/command/user/ChooseClassCmd.java
@@ -7,10 +7,20 @@ package com.valygard.KotH.command.user;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
+import com.valygard.KotH.KotH;
 import com.valygard.KotH.KotHUtils;
 import com.valygard.KotH.command.Command;
 import com.valygard.KotH.command.CommandInfo;
@@ -21,13 +31,9 @@ import com.valygard.KotH.framework.ArenaManager;
 import com.valygard.KotH.messenger.Messenger;
 import com.valygard.KotH.messenger.Msg;
 import com.valygard.KotH.player.ArenaClass;
+import com.valygard.KotH.util.ItemParser;
 
-@CommandInfo(
-		name = "chooseclass", 
-		pattern = "(choose|pick)class.*|class",
-		desc = "Choose a class",
-		playerOnly = true
-	)
+@CommandInfo(name = "chooseclass", pattern = "(choose|pick)class.*|class", desc = "Choose a class", playerOnly = true, argsRequired = 0)
 @CommandPermission("koth.user.pickclass")
 @CommandUsage("/koth chooseclass <class|random>")
 /**
@@ -40,38 +46,51 @@ public class ChooseClassCmd implements Command {
 	public boolean execute(ArenaManager am, CommandSender sender, String[] args) {
 		Player p = (Player) sender;
 		Arena arena = am.getArenaWithPlayer(p);
-		
+
 		if (arena == null) {
 			Messenger.tell(p, "You are not in an arena!");
 			return false;
 		}
+
+		if (!arena.inLobby(p)) {
+			Messenger.tell(p, Msg.MISC_NO_ACCESS);
+			return true;
+		}
 		
-        if (!arena.inLobby(p)) {
-            Messenger.tell(p, Msg.MISC_NO_ACCESS);
-            return true;
-        }
+		if (args.length == 0) {
+			if (arena.getSettings().getBoolean("classes-gui")) {
+				GUI gui = new GUI(am, p);
+				am.getPlugin().getServer().getPluginManager().registerEvents(gui, am.getPlugin());
+				gui.openInv();
+				return true;
+			} else {
+				showAvailableClasses(am, p);
+				return true;
+			}
+		}
+		
+		String lowercase = args[0].toLowerCase();
+		ArenaClass ac = am.getClasses().get(lowercase);
 
-        String lowercase = args[0].toLowerCase();
-        ArenaClass ac = am.getClasses().get(lowercase);
-        
-        if (ac == null && !lowercase.equals("random")) {
+		if (ac == null && !lowercase.equals("random")) {
 			showAvailableClasses(am, p);
-            return true;
-        }
+			return true;
+		}
 
-        if (!am.getPlugin().has(p, "koth.classes." + lowercase) && !lowercase.equals("random")) {
-            Messenger.tell(p, Msg.CLASS_NO_ACCESS);
-            showAvailableClasses(am, p);
-            return true;
-        }
-        
-        if (!lowercase.equals("random")) {
-        	arena.pickClass(p, lowercase);
-        	Messenger.tell(p, Msg.CLASS_CHOSEN, lowercase);
-        } else {
-        	arena.giveRandomClass(p);
-        }
-        return true;
+		if (!am.getPlugin().has(p, "koth.classes." + lowercase)
+				&& !lowercase.equals("random")) {
+			Messenger.tell(p, Msg.CLASS_NO_ACCESS);
+			showAvailableClasses(am, p);
+			return true;
+		}
+
+		if (!lowercase.equals("random")) {
+			arena.pickClass(p, lowercase);
+			Messenger.tell(p, Msg.CLASS_CHOSEN, lowercase);
+		} else {
+			arena.giveRandomClass(p);
+		}
+		return true;
 	}
 
 	private void showAvailableClasses(ArenaManager am, Player p) {
@@ -84,7 +103,115 @@ public class ChooseClassCmd implements Command {
 					+ s.toLowerCase() + ChatColor.RESET + ",");
 		}
 
-        String result = KotHUtils.formatList(classes, am.getPlugin());
-        Messenger.tell(p, Msg.MISC_LIST_CLASSES, result);
+		String result = KotHUtils.formatList(classes, am.getPlugin());
+		Messenger.tell(p, Msg.MISC_LIST_CLASSES, result);
+	}
+	
+	
+	
+	
+	
+	
+	
+
+	private class GUI implements Listener {
+		private ArenaManager am;
+		private KotH plugin;
+		private Player player;
+
+		private Inventory inv;
+
+		private int classSize;
+		private ConfigurationSection classes;
+
+		public GUI(ArenaManager am, Player player) {
+			this.am = am;
+			this.plugin = am.getPlugin();
+			
+			this.player = player;
+
+			this.classSize = am.getClasses().size();
+			this.classes = plugin.getConfig()
+					.getConfigurationSection("classes");
+			
+			this.inv = Bukkit.createInventory(null, calculateInvSize(),
+					"Available Classes");
+			
+			Bukkit.getPluginManager().registerEvents(this, plugin);
+			
+			openInv();
+		}
+
+		private int calculateInvSize() {
+			for (int i = 9; i <= 54; i += 9) {
+				if (classSize <= i)
+					return i;
+			}
+			return 54;
+		}
+
+		private void openInv() {
+			int slot = 0;
+			for (String className : classes.getKeys(false)) {
+				String gui = plugin.getConfig().getString(
+						"classes." + className + ".gui");
+				
+				ItemStack stack;
+				if (gui == null || gui.isEmpty()) {
+					stack = new ItemStack(Material.GRASS, 1);
+				} else {
+					// If the ItemParser detects more than one item, set to grass.
+					stack = gui.contains(",") ? new ItemStack(Material.GRASS, 1) : ItemParser.parseItem(gui);
+				}
+				
+				ItemMeta im = stack.getItemMeta();
+				im.setDisplayName((player.hasPermission("koth.classes."
+						+ className.toLowerCase()) ? ChatColor.DARK_GREEN
+						: ChatColor.GRAY) + className);
+				stack.setItemMeta(im);
+				
+				inv.setItem(slot, stack);
+				slot++;
+				
+				if (classSize == slot) {
+					break;
+				}
+			}
+			player.openInventory(inv);
+		}
+		
+		@EventHandler
+		public void onInventoryClick(InventoryClickEvent e) {
+			if (!e.getInventory().getTitle().equalsIgnoreCase("Available Classes")) {
+				return;
+			}
+			
+			player = (Player) e.getWhoClicked();
+			ItemStack stack = e.getCurrentItem();
+			
+			if (e.getCurrentItem() == null || e.getCurrentItem().getType() == Material.AIR) {
+				e.setCancelled(true);
+				return;
+			}
+			
+			Arena arena = am.getArenaWithPlayer(player);
+			// No null checks required on arena
+			
+			if (!stack.getItemMeta().hasDisplayName()) {
+				e.setCancelled(true);
+				player.closeInventory();
+				return;
+			}
+			
+			String lowercase = ChatColor.stripColor(stack.getItemMeta().getDisplayName()).toLowerCase();
+			if (player.hasPermission("koth.classes." + lowercase)) {
+				arena.pickClass(player, lowercase);
+				Messenger.tell(player, Msg.CLASS_CHOSEN, lowercase);
+			} else {
+				arena.giveRandomClass(player);
+			}
+			e.setCancelled(true);
+			player.closeInventory();
+		}
 	}
 }
