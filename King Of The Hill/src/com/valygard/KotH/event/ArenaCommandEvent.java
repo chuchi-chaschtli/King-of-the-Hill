5bdbdd6536eefc48ebee93402963893e44db3bd5
AoHRuthless <kumara0398@gmail.com>
2014-07-31 09:56:43 -0400

    Add new functionalities to ArenaCommandEvent. Developer can now access the arguments in one string instead of a string array, and now can use boolean #isPlayer() check to discern if the sender is a player.

--- King Of The Hill/src/com/valygard/KotH/event/ArenaCommandEvent.java
+++ King Of The Hill/src/com/valygard/KotH/event/ArenaCommandEvent.java
@@ -4,6 +4,7 @@
 package com.valygard.KotH.event;
 
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
@@ -12,11 +13,11 @@ import com.valygard.KotH.util.StringUtils;
 
 /**
  * @author Anand
- * 
+ * @since 1.2.5
  */
 public class ArenaCommandEvent extends Event implements Cancellable {
 	private static final HandlerList handlers = new HandlerList();
-	
+
 	private CommandSender sender;
 	private String command;
 
@@ -28,7 +29,7 @@ public class ArenaCommandEvent extends Event implements Cancellable {
 		this.command = command;
 		this.cancelled = false;
 	}
-	
+
 	/**
 	 * Obtains the sender of the command.
 	 * 
@@ -37,38 +38,58 @@ public class ArenaCommandEvent extends Event implements Cancellable {
 	public CommandSender getSender() {
 		return sender;
 	}
-	
+
+	/**
+	 * Checks if the command sender is a player or not.
+	 * 
+	 * @since 1.2.11
+	 * @return true if the sender is a player, false otherwise.
+	 */
+	public boolean isPlayer() {
+		return (sender instanceof Player);
+	}
+
 	/**
 	 * Gets the command a sender used.
 	 * 
-	 * @since v1.2.5
-	 * @return a string. If the sender types /koth HI, this will return "koth HI"
+	 * @return a string. If the sender types /koth HI, this will return
+	 *         "koth HI"
 	 */
 	public String getCommand() {
 		return command;
 	}
-	
+
 	/**
 	 * Gets the command a sender used in all lowercase.
 	 * 
-	 * @since v1.2.5
-	 * @return a string. If the sender types /koth HI, this will return "koth hi".
-	 * @return
+	 * @return a string. If the sender types /koth HI, this will return
+	 *         "koth hi".
 	 */
 	public String getLowercaseCommand() {
 		return command.toLowerCase();
 	}
-	
+
 	/**
 	 * Gets the arguments of the command used by the sender.
 	 * 
-	 * @since v1.2.5
-	 * @return a string array. If the sender types "/koth HI there", this will return 'HI, there'.
+	 * @return a string array. If the sender types "/koth HI there", this will
+	 *         return HI as args[0] and there as args[1]
 	 */
 	public String[] getArgs() {
 		return StringUtils.trimByRegex(command, " ", 1).split(" ");
 	}
-	
+
+	/**
+	 * Gets the arguments of command as one string.
+	 * 
+	 * @since 1.2.11
+	 * @return a String of arguments. If sender types command /koth a b c, this
+	 *         will return 'a b c'.
+	 */
+	public String getFormattedArgs() {
+		return StringUtils.trimByRegex(command, " ", 1);
+	}
+
 	@Override
 	public HandlerList getHandlers() {
 		return handlers;
