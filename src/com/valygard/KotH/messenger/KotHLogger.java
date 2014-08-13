10ce8971d9d06c3d73fdb83a963719a7af37b7fe
AoHRuthless <kumara0398@gmail.com>
2014-08-13 07:32:24 -0400

    Add default error message.

--- src/com/valygard/KotH/messenger/KotHLogger.java
+++ src/com/valygard/KotH/messenger/KotHLogger.java
@@ -20,57 +20,67 @@ import org.bukkit.plugin.Plugin;
  */
 public class KotHLogger {
 	private Plugin plugin;
-	
+
 	private static final Logger LOGGER = Logger.getLogger("Minecraft");
 	private static final String PREFIX = "[KotH] ";
-	
+
 	public KotHLogger(Plugin plugin) {
 		this.plugin = plugin;
-		
+
 		info("Logger successfully initialized!");
 	}
-	
-	private void logMessage(String level, String msg) {	
+
+	private void logMessage(String level, String msg) {
 		if (plugin.getConfig().getBoolean("global.logging")) {
 			return;
 		}
-		
+
 		File dataFolder = plugin.getDataFolder();
 		try {
-			if(!dataFolder.exists()) {
+			if (!dataFolder.exists()) {
 				dataFolder.mkdir();
 			}
-			
-			File file = new File(dataFolder, "KotH.log") ;
-			if(!file.exists()){
-				file.createNewFile();		
+
+			File file = new File(dataFolder, "KotH.log");
+			if (!file.exists()) {
+				file.createNewFile();
 			}
-			
+
 			Date date = new Date();
 			SimpleDateFormat df = new SimpleDateFormat("[MM-dd-yyyy HH:mm:ss]");
 			String time = df.format(date);
-			
-			FileWriter fw = new FileWriter(file, true);	  
+
+			FileWriter fw = new FileWriter(file, true);
 			PrintWriter pw = new PrintWriter(fw);
-			pw.println(time + " " + PREFIX +  "[" + level.toUpperCase() + "] : " + msg);
+			pw.println(time + " " + PREFIX + "[" + level.toUpperCase() + "] : "
+					+ msg);
 			pw.close();
-		} catch (IOException e) {
+		}
+		catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void info(String msg) {
 		logMessage("info", msg);
 		LOGGER.log(Level.INFO, msg);
 	}
-	
+
 	public void warn(String msg) {
 		LOGGER.log(Level.WARNING, msg);
 		logMessage("warn", msg);
 	}
-	
+
 	public void error(String msg) {
 		LOGGER.log(Level.SEVERE, msg);
 		logMessage("severe", msg);
+	}
+
+	public void error() {
+		String errMsg = "ERROR FOUND! Tell AoH_Ruthless on the issue tracker that he screwed up! "
+				+ "Be sure to include the error log."
+				+ '\n'
+				+ "https://github.com/AoHRuthless/King-of-the-Hill/issues/new";
+		error(errMsg);
 	}
 }
