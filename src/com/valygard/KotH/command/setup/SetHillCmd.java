0b00b81a699611a53fac0fd934255a677a02d5ee
AoHRuthless <kumara0398@gmail.com>
2014-08-14 02:32:27 -0400

    Update hill location math to properly set hills on the closest whole block to the player.

--- src/com/valygard/KotH/command/setup/SetHillCmd.java
+++ src/com/valygard/KotH/command/setup/SetHillCmd.java
@@ -20,12 +20,7 @@ import com.valygard.KotH.messenger.Messenger;
 import com.valygard.KotH.messenger.Msg;
 import com.valygard.KotH.util.ConfigUtil;
 
-@CommandInfo(
-		name = "sethill", 
-		pattern = "(add|set)hill.*",
-		desc = "Set a new hill for an arena on your current location or override an existing one.",
-		playerOnly = true
-)
+@CommandInfo(name = "sethill", pattern = "(add|set)hill.*", desc = "Set a new hill for an arena on your current location or override an existing one.", playerOnly = true)
 @CommandPermission("koth.setup.sethill")
 @CommandUsage("/koth sethill <arena> [hill#]")
 /**
@@ -37,48 +32,55 @@ public class SetHillCmd implements Command {
 	@Override
 	public boolean execute(ArenaManager am, CommandSender sender, String[] args) {
 		Player p = (Player) sender;
-		
+
 		Arena arena = am.getArenaWithName(args[0]);
 		if (arena == null) {
 			Messenger.tell(p, Msg.ARENA_NULL);
 			return true;
 		}
-		
+
 		if (arena.isRunning()) {
 			Messenger.tell(p, Msg.ARENA_IN_PROGRESS);
 			return true;
 		}
-		
+
 		if (!p.getLocation().getWorld().equals(arena.getWorld())) {
 			Messenger.tell(p, Msg.MISC_WRONG_WORLD, arena.getWorld().getName());
 			return true;
 		}
-		
+
 		ConfigurationSection warps = arena.getWarps();
 		if (warps == null) {
-			warps = am.getConfig().createSection("arenas." + args[0] + ".warps");
+			warps = am.getConfig()
+					.createSection("arenas." + args[0] + ".warps");
 			am.saveConfig();
 		}
-		
-		double x = p.getLocation().getX() + 0.5;
-		double z = p.getLocation().getZ() + 0.5;
-		Location l = new Location(p.getWorld(), x, p.getLocation().getBlockY(), z);
-		
+
+		double x = Math.round(p.getLocation().getX());
+		double z = Math.round(p.getLocation().getZ());
+
+		x += x < 0 ? -0.5 : 0.5;
+		z += z < 0 ? -0.5 : 0.5;
+
+		Location l = new Location(p.getWorld(), x, p.getLocation().getBlockY(),
+				z);
+
 		ConfigurationSection s = warps.getConfigurationSection("hills");
 		if (args.length == 1) {
 			if (s == null) {
 				arena.getWarps().createSection("hills");
-				arena.getWarps().getConfigurationSection("hills").set(String.valueOf(1), "");
+				arena.getWarps().getConfigurationSection("hills")
+						.set(String.valueOf(1), "");
 				am.saveConfig();
 				ConfigUtil.setLocation(s, String.valueOf(1), l);
 			} else {
 				for (int i = 0; i <= s.getKeys(false).size(); i++) {
-					// Sanity Checks	
+					// Sanity Checks
 					if (i == 0 && s.getString(String.valueOf(1)) == null) {
-						if(!callHillEvent(arena, p)) {
+						if (!callHillEvent(arena, p)) {
 							return true;
 						}
-						
+
 						ConfigUtil.setLocation(s, String.valueOf(1), l);
 						Messenger.tell(p, Msg.HILLS_ADDED);
 						break;
@@ -88,19 +90,21 @@ public class SetHillCmd implements Command {
 						continue;
 
 					ConfigUtil.setLocation(s, String.valueOf(i + 1), l);
-					
-					if(!callHillEvent(arena, p)) {
+
+					if (!callHillEvent(arena, p)) {
 						return true;
 					}
 
-					if (s.getString(String.valueOf(i)).equals(s.getString(String.valueOf(i + 1)))) {
-						Messenger.tell(p, "There is already a hill at this location.");
+					if (s.getString(String.valueOf(i)).equals(
+							s.getString(String.valueOf(i + 1)))) {
+						Messenger.tell(p,
+								"There is already a hill at this location.");
 						s.set(String.valueOf(i + 1), null);
 						am.saveConfig();
 						return true;
 					}
-					
-					if(!callHillEvent(arena, p)) {
+
+					if (!callHillEvent(arena, p)) {
 						return true;
 					}
 
@@ -112,24 +116,25 @@ public class SetHillCmd implements Command {
 			int number = Integer.parseInt(args[1]);
 			if (s.getString(String.valueOf(number)) != null) {
 				ConfigUtil.setLocation(s, String.valueOf(number), l);
-				
+
 				if (!callHillEvent(arena, p)) {
 					return true;
 				}
-				
+
 				Messenger.tell(p, Msg.HILLS_RESET, String.valueOf(number));
 			} else {
-				Messenger.tell(p, "There is no hill with the specified number.");
+				Messenger
+						.tell(p, "There is no hill with the specified number.");
 				return true;
 			}
 		}
 		am.saveConfig();
 		am.reloadArena(arena);
 		am.tellHowManyMissing(arena, p);
-		
+
 		return true;
 	}
-	
+
 	private boolean callHillEvent(Arena arena, Player p) {
 		HillCreateEvent event = new HillCreateEvent(arena, p);
 		Bukkit.getPluginManager().callEvent(event);
