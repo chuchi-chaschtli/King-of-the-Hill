827d83123211804316f47551be4bdfb3d7c1f263
AoHRuthless <kumara0398@gmail.com>
2016-06-12 16:51:15 -0400

    Remove obsolete utility method

--- src/com/valygard/KotH/util/StringUtils.java
+++ src/com/valygard/KotH/util/StringUtils.java
@@ -4,8 +4,6 @@
 package com.valygard.KotH.util;
 
 import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
 
 import org.bukkit.entity.Player;
 
@@ -66,26 +64,6 @@ public class StringUtils {
 		}
 
 		return buffy.toString().substring(0, buffy.length() - trimLength);
-	}
-
-	/**
-	 * Format a string into list form.
-	 * 
-	 * @param list
-	 *            the string.
-	 * @return a string list.
-	 */
-	public static List<String> formatList(String list) {
-		List<String> result = new LinkedList<String>();
-		if (list == null)
-			return result;
-
-		String[] parts = list.trim().split(",");
-
-		for (String part : parts)
-			result.add(part.trim());
-
-		return result;
 	}
 
 	/**
