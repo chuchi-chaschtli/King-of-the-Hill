d0e7aaed1cd8378093f12ad973750ecd33b679a9
AoHRuthless <kumara0398@gmail.com>
2016-06-28 23:33:35 -0400

    Stats command now shows top 10 most classes used.

--- src/com/valygard/KotH/command/user/StatsCmd.java
+++ src/com/valygard/KotH/command/user/StatsCmd.java
@@ -3,17 +3,14 @@
  */
 package com.valygard.KotH.command.user;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import com.valygard.KotH.KotHUtils;
 import com.valygard.KotH.command.Command;
 import com.valygard.KotH.command.CommandInfo;
 import com.valygard.KotH.command.CommandPermission;
@@ -26,12 +23,7 @@ import com.valygard.KotH.player.ArenaClass;
 import com.valygard.KotH.player.PlayerStats;
 import com.valygard.KotH.util.StringUtils;
 
-@CommandInfo(
-		name = "stats", 
-		pattern = "a.*stats|stats",
-		desc = "View your statistics for an arena.",
-		playerOnly = true
-	)
+@CommandInfo(name = "stats", pattern = "a.*stats|stats", desc = "View your statistics for an arena.", playerOnly = true)
 @CommandPermission("koth.user.arenastats")
 @CommandUsage("/koth stats <arena>")
 /**
@@ -44,25 +36,25 @@ public class StatsCmd implements Command {
 	public boolean execute(ArenaManager am, CommandSender sender, String[] args) {
 		Player p = (Player) sender;
 		Arena arena = am.getArenaWithName(args[0]);
-		
+
 		if (arena == null) {
 			Messenger.tell(p, Msg.ARENA_NULL);
 			return true;
 		}
-		
+
 		PlayerStats stats = arena.getStats(p);
-		
+
 		if (stats == null) {
 			Messenger.tell(p, Msg.STATS_NULL);
 			return true;
 		}
-		
+
 		// Some variables
-		String kills 	= makeString(stats.getKills()) + "kills and";
-		String deaths 	= makeString(stats.getDeaths()) + "deaths.";
-		String wins 	= makeString(stats.getWins()) + "wins,";
-		String losses 	= makeString(stats.getLosses()) + "losses and";
-		String draws 	= makeString(stats.getDraws()) + "draws.";
+		String kills = makeString(stats.getKills()) + "kills and";
+		String deaths = makeString(stats.getDeaths()) + "deaths.";
+		String wins = makeString(stats.getWins()) + "wins,";
+		String losses = makeString(stats.getLosses()) + "losses and";
+		String draws = makeString(stats.getDraws()) + "draws.";
 
 		// This insures that the double is formatted.
 		String kdr = "Your KDR is"
@@ -71,35 +63,34 @@ public class StatsCmd implements Command {
 				+ makeString(stats, stats.getWins(), stats.getLosses());
 
 		// Streaks
-		String killstreak 	= (stats.getKillstreak() > 0 ? "You are on a"
+		String killstreak = (stats.getKillstreak() > 0 ? "You are on a"
 				+ makeString(stats.getKillstreak()) + "killstreak." : "");
-		String winstreak 	= (stats.getWinstreak() > 0 ? "You are on a"
+		String winstreak = (stats.getWinstreak() > 0 ? "You are on a"
 				+ makeString(stats.getWinstreak()) + "winstreak." : "");
-		
-		// Sorted class-data through comparator
-		final Map<String, Integer> classData = new HashMap<String, Integer>();
-		
+
+		// Sorted class-data
+		Map<String, Integer> classData = new HashMap<String, Integer>();
+
 		for (ArenaClass ac : am.getClasses().values()) {
 			int timesUsed = stats.getClassData().getInt(ac.getLowercaseName());
 			classData.put(ac.getLowercaseName(), timesUsed);
 		}
-		
-		List<String> list = new ArrayList<String>(classData.keySet());
-		
-		Comparator<String> cmp = new Comparator<String>() {
-			@Override
-			public int compare(String s1, String s2) {
-				Integer timesUsed1 = classData.get(s1);
-				Integer timesUsed2 = classData.get(s2);
-				return timesUsed1.compareTo(timesUsed2);
+		classData = KotHUtils.sortMapByValue(classData, false);
+
+		int counter = 0;
+		StringBuilder classes = new StringBuilder();
+		for (String arenaClass : classData.keySet()) {
+			counter++;
+			classes.append("You have used class '" + ChatColor.YELLOW
+					+ arenaClass + ChatColor.RESET + "' a total of "
+					+ ChatColor.YELLOW + classData.get(arenaClass)
+					+ (classData.get(arenaClass) == 1 ? " time." : " times."));
+			classes.append(ChatColor.RESET).append("\n");
+
+			if (counter >= 9 || classData.size() == counter) {
+				break;
 			}
-		};
-		
-		Collections.sort(list, Collections.reverseOrder(cmp));
-		int amount = classData.get(list.get(0));
-		String mostUsed = "You have used class '" + ChatColor.YELLOW
-				+ list.get(0) + ChatColor.RESET + "' a total of "
-				+ ChatColor.YELLOW + amount + (amount == 1 ? " time." : " times.");
+		}
 
 		// Pretty up the time spent in the arena.
 		String timespent = "You have spent" + ChatColor.YELLOW
@@ -113,9 +104,10 @@ public class StatsCmd implements Command {
 		foo.append("\n").append("You have").append(kills).append(deaths);
 		foo.append("\n").append("You have").append(wins).append(losses)
 				.append(draws);
-		
-		foo = StringUtils.appendWithNewLines(foo, " ", kdr, wlr, " ", killstreak, winstreak, " ", timespent, " ", mostUsed);
-		
+
+		foo = StringUtils.appendWithNewLines(foo, " ", kdr, wlr, " ",
+				killstreak, winstreak, " ", timespent, " ", classes.toString());
+
 		Messenger.tell(p, Msg.STATS, foo.toString());
 		return true;
 	}
