454678d2ce7eaaf6944c9154f4a8d672ae3f522f
AoHRuthless <kumara0398@gmail.com>
2016-08-12 23:08:31 -0400

    Decrease MMR fluctuation constant

--- src/com/valygard/KotH/matchmaking/KotHRatingSystem.java
+++ src/com/valygard/KotH/matchmaking/KotHRatingSystem.java
@@ -202,12 +202,12 @@ public class KotHRatingSystem {
 		double playFactor = played * 0.01D;
 
 		if (rating >= base) {
-			return 0.05D / (scoreFactor + (played * 0.01D));
+			return 0.04D / (scoreFactor + playFactor);
 		}
 		if (rating >= 0) {
-			return (0.001D / (0.03D - (0.01D * scoreFactor) + (playFactor * 0.005D)));
+			return (0.001D / (0.035D - (0.01D * scoreFactor) + (playFactor * 0.0035D)));
 		}
-		return (0.001D / (-0.03D - (0.01D * scoreFactor) + (playFactor * 0.005D)));
+		return (0.001D / (-0.035D - (0.01D * scoreFactor) + (playFactor * 0.0035D)));
 	}
 
 	/**
